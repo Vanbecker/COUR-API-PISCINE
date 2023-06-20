5a47cf5446cb235efd1c36f680dc585dd36fb8a8
Rémi Le Gall <126484877+Vanbecker@users.noreply.github.com>
2023-06-20 16:17:00 +0200

    catastrophique

--- starter/app.js
+++ starter/app.js
@@ -1,8 +1,16 @@
+const btn = document.querySelector('button')
+const dataset = 'bot_frequentation_piscine_tr'
+const errorMsg = document.querySelector("errorMsg")
+
+
+btn.addEventListener('click', onBtnClick)
+
 async function fetchData() {
     try {
         const response = await fetch(`https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=${dataset}`)
 
         if (!response.ok) {
+            changeColorBtn('blue')
             throw new Error(`Erreur: ${response.status}`)
         }
 
@@ -12,7 +20,40 @@ async function fetchData() {
         // displayData(data);
     }
     catch (error) {
+        changeColorBtn('red')
         // btnChange()
-        // errorMsg.textContent = `${error}`
+        errorMsg.textContent = `${error}`
     }
+
+    function btnChange(type) {
+        if (type === 'fetching') {
+
+
+
+        }
+    }
+    function onBtnClick() {
+        changeColorBtn('blue')
+        fetchData()
+    }
+
+    function changeColorBtn(couleur) {
+        btn.style.backgroundColor = couleur
+    }
+
 }
+
+
+
+
+
+
+// let min = 0
+// let middle = 40
+// let max = 100
+// let random = Math.random(max || middle || min);
+// // alert(random);
+// console.log(random)
+
+
+
